e096a90c4b07d39662b4fd6e5776a9329bc3eb82
Denis Golubovskiy <bukashk0zzz@gmail.com>
2014-12-17 11:10:09 +0200

    show temperature hum and motion

--- temperature/storeData.php
+++ temperature/storeData.php
@@ -6,14 +6,12 @@ if (!$db) exit("Can not create database.");
 $status = false;
 $data = exec("./readDHT");
 
-while (!$status) {
-    if ($data) {
-        $data = explode(';',$data);
-        $t = trim($data[0]);
-        $h = trim($data[1]);
-        $time = time();
-        $db->exec("INSERT INTO temperature(temperature, humidity, time) VALUES ($t, $h, $time);");
-        $status = true;
-    }
+if ($data) {
+    $data = explode(';',$data);
+    $t = trim($data[0]);
+    $h = trim($data[1]);
+    $time = time();
+    $db->exec("INSERT INTO temperature(temperature, humidity, time) VALUES ($t, $h, $time);");
+    $status = true;
 }
 
