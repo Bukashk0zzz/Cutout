2580b30568c33fe5ee0deb09ca18171829af757c
Denis Golubovskiy <bukashk0zzz@gmail.com>
2014-12-17 10:25:04 +0200

    fix db

--- temperature/createDb.php
+++ temperature/createDb.php
@@ -1,12 +1,12 @@
 <?php
-// Создадим новую базу данных
-$db = sqlite_open("temperature.db");
-if (!$db) exit("Невозможно создать базу данных!");
 
-$query_table = sqlite_query($db, "CREATE TABLE temperature
+$db = new SQLite3("temperature.db");
+if (!$db) exit("Can not create database.");
+
+$query_table = $db->exec("CREATE TABLE temperature
                               (id INTEGER PRIMARY KEY,
                                temperature REAL,
                                humidity REAL,
                                time INTEGER);
                               ");
-if (!$query_table) exit("Невозможно создать таблицу в базе данных!");
+if (!$query_table) exit("Can not create table.");
