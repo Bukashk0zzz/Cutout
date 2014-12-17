e493f91c6d7212c70b4a949eb99efb4fd034caf6
Denis Golubovskiy <bukashk0zzz@gmail.com>
2014-12-17 10:11:24 +0200

    some improvements

--- temperature/readDHT.c
+++ temperature/readDHT.c
@@ -1,5 +1,5 @@
 #define BCM2708_PERI_BASE        0x20000000
-#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000)
 
 
 #include <stdio.h>
@@ -28,118 +28,123 @@ int readDHT(int type, int pin);
 
 int main(int argc, char **argv)
 {
-  if (!bcm2835_init())
-        return 1;
+    if (!bcm2835_init())
+    return 1;
+    
+    int type = 0;
+    type = DHT22;
+    int dhtpin = 3;
+    
+    #ifdef DEBUG
+        printf("Using pin #%d n", dhtpin);
+    #endif
 
-  int type = 0;
-  type = DHT22;
-  
-  int dhtpin = 3;
-
-
-//  printf("Using pin #%d\n", dhtpin);
-  int i = 10;
-  while (--i && readDHT(type, dhtpin))
- {
-   printf("%d",i);
- }
-  bcm2835_close();
-  return 0;
-
-} // main
+    int i = 10;
+    while (--i && readDHT(type, dhtpin))
+    {
+        printf("Try number %d n",i);
+    }
+    bcm2835_close();
+    return 0;
+    
+}
 
 
 int bits[250], data[100];
 int bitidx = 0;
 
 int readDHT(int type, int pin) {
-  int counter = 0;
-  int laststate = HIGH;
-  int j=0;
-
-  // Set GPIO pin to output
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
-
-  bcm2835_gpio_write(pin, HIGH);
-  usleep(500000);  // 500 ms
-  bcm2835_gpio_write(pin, LOW);
-  usleep(20000);
-
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
-  bcm2835_gpio_set_pud (pin, BCM2835_GPIO_PUD_UP);
-
-  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
-
-  // wait for pin to drop?
-  int cnt = 0;
-  while (bcm2835_gpio_lev(pin) == 1) {
-    cnt++;
-    usleep(1);
-    if (cnt == 1000)
-      break;
-  }
-
-  // read data!
-  for (int i=0; i< MAXTIMINGS; i++) {
-    counter = 0;
-    while ( bcm2835_gpio_lev(pin) == laststate) {
-	counter++;
-	//nanosleep(1);		// overclocking might change this?
-        if (counter == 1000)
-	  break;
+    int counter = 0;
+    int laststate = HIGH;
+    int j=0;
+    
+    // Set GPIO pin to output
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
+    
+    bcm2835_gpio_write(pin, HIGH);
+    usleep(500000);  // 500 ms
+    bcm2835_gpio_write(pin, LOW);
+    usleep(20000);
+    
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud (pin, BCM2835_GPIO_PUD_UP);
+    
+    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+    
+    // wait for pin to drop?
+    int cnt = 0;
+    while (bcm2835_gpio_lev(pin) == 1) {
+        cnt++;
+        usleep(1);
+        if (cnt == 1000)
+        break;
     }
-    laststate = bcm2835_gpio_lev(pin);
-    if (counter == 1000) break;
-#ifdef DEBUG
-    bits[bitidx++] = counter;
-#endif
-
-    if ((i>3) && (i%2 == 0)) {
-      // shove each bit into the storage bytes
-      data[j/8] <<= 1;
-      if (counter > 200)
-        data[j/8] |= 1;
-      j++;
+    
+    // read data!
+    for (int i=0; i< MAXTIMINGS; i++) {
+        counter = 0;
+        while ( bcm2835_gpio_lev(pin) == laststate) {
+            counter++;
+            //nanosleep(1);        // overclocking might change this?
+            if (counter == 1000)
+            break;
+        }
+        laststate = bcm2835_gpio_lev(pin);
+        if (counter == 1000) break;
+        #ifdef DEBUG
+            bits[bitidx++] = counter;
+        #endif
+        
+        if ((i>3) && (i%2 == 0)) {
+            // shove each bit into the storage bytes
+            data[j/8] <<= 1;
+            if (counter > 200)
+            data[j/8] |= 1;
+            j++;
+        }
     }
-  }
+    
+    
+    #ifdef DEBUG
+        for (int i=3; i<bitidx; i+=2) {
+            printf("bit %d: %dn", i-3, bits[i]);
+            printf("bit %d: %d (%d)n", i-2, bits[i+1], bits[i+1] > 200);
+        }
+        printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%xn", j, data[0], data[1], data[2], data[3], data[4]);
+    #endif
+    
+    if ((j >= 39) &&
+    (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
 
+        if (type == DHT11) {
+            printf("%d ; %d n", data[2], data[0]);
+        }
 
-#ifdef DEBUG
-  for (int i=3; i<bitidx; i+=2) {
-    printf("bit %d: %d\n", i-3, bits[i]);
-    printf("bit %d: %d (%d)\n", i-2, bits[i+1], bits[i+1] > 200);
-  }
-#endif
+        if (type == DHT22) {
+            float f, h;
+            h = data[0] * 256 + data[1];
+            h /= 10;
+            
+            f = (data[2] & 0x7F)* 256 + data[3];
+            f /= 10.0;
+            if (data[2] & 0x80)  f *= -1;
+            printf("%.1f ; %.1f n", f, h);
+            
+        }
+        return 0;
+    } else {
+        #ifdef DEBUG
+            printf("CRC error!n");
+            printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%xn", j, data[0], data[1], data[2], data[3], data[4]);
 
-//  printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%x\n", j, data[0], data[1], data[2], data[3], data[4]);
-  if ((j >= 39) &&
-      (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
-     // yay!
-     if (type == DHT11)
-	printf("%d ; %d \%\n", data[2], data[0]);
-     if (type == DHT22) {
-	float f, h;
-	h = data[0] * 256 + data[1];
-	h /= 10;
+            for (int i=3; i<bitidx; i+=2) {
+                printf("bit %d: %dn", i-3, bits[i]);
+                printf("bit %d: %d (%d)n", i-2, bits[i+1], bits[i+1] > 200);
+            }
+        #endif
 
-	f = (data[2] & 0x7F)* 256 + data[3];
-        f /= 10.0;
-        if (data[2] & 0x80)  f *= -1;
-	printf("%.1f ; %.1f \n", f, h);
-
+        return 1;
     }
+    
     return 0;
-  } else {
-     printf("CRC error!\n");
-//     printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%x\n", j, data[0], data[1], data[2], data[3], data[4]);
-  for (int i=3; i<bitidx; i+=2) {
-        printf("bit %d: %d\n", i-3, bits[i]);
-        printf("bit %d: %d (%d)\n", i-2, bits[i+1], bits[i+1] > 200);
-     }
-
-     return 1;
-  }
-
-  return 0;
 }
-
